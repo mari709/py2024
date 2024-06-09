c6ded35f0aaa04ffe8793fe90addbde5cfefec4d
Marina Mansueto <mansumdp@gmail.com>
2024-06-09 15:09:56 -0300

    1,2 y 3

---
+++ html-css/python/frutas.py
@@ -0,0 +1,9 @@
+frutas= ['manzana', 'banana', 'frutilla']
+
+frutas[0]="kiwi"
+print(frutas)
+
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+print(fruits)  # Salida: ['apple', 'banana', 'cherry', 'orange']
+
